94bdb75aa8ffd6fcd4073b0190f679472df91f46
Charlie Moore <moore.c@wustl.edu>
2021-06-30 16:51:42 -0500

    XNAT-6832: added new message field introduced to core XNAT's SessionData

--- src/main/groovy/org/nrg/xnat/pogo/dicom/SessionData.groovy
+++ src/main/groovy/org/nrg/xnat/pogo/dicom/SessionData.groovy
@@ -4,7 +4,7 @@ import com.fasterxml.jackson.annotation.JsonFormat
 
 class SessionData {
     @JsonFormat(pattern='yyyy-MM-dd HH:mm:ss.SSS') Date uploadDate, lastBuiltDate, scan_date
-    String scan_time, subject, url, session, tag, source, visit, protocol, timeZone, timestamp, project, folderName, status, externalUrl, name
+    String scan_time, subject, url, session, tag, source, visit, protocol, timeZone, timestamp, project, folderName, status, externalUrl, name, message
     Boolean preventAnon = false
     Boolean preventAutoCommit = false
     Long id
