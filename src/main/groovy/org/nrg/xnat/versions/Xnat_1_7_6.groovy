d41a797f5f25b22c237b75f68d1c15b94438e725
Charlie Moore <moore.c@wustl.edu>
2021-08-09 17:24:33 -0500

    Added version class for 1.7.5.2 for selenium tests

--- src/main/groovy/org/nrg/xnat/versions/Xnat_1_7_6.groovy
+++ src/main/groovy/org/nrg/xnat/versions/Xnat_1_7_6.groovy
@@ -3,7 +3,7 @@ package org.nrg.xnat.versions
 import org.nrg.xnat.interfaces.XnatInterface
 import org.nrg.xnat.interfaces.XnatInterface_1_8_0
 
-@Follows(Xnat_1_7_5)
+@Follows(Xnat_1_7_5_2)
 class Xnat_1_7_6 extends XnatVersion {
 
     @Override
