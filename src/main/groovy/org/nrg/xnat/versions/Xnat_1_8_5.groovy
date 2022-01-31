682f830cac464fcb987f37149096ba32e357cefe
Charlie Moore <moore.c@wustl.edu>
2022-01-31 10:14:02 -0600

    Add 1.8.5 class

---
+++ src/main/groovy/org/nrg/xnat/versions/Xnat_1_8_5.groovy
@@ -0,0 +1,19 @@
+package org.nrg.xnat.versions
+
+import org.nrg.xnat.interfaces.XnatInterface
+import org.nrg.xnat.interfaces.XnatInterface_1_8_0
+
+@Follows(Xnat_1_8_4)
+class Xnat_1_8_5 extends XnatVersion {
+
+    @Override
+    List<String> getVersionKeys() {
+        ['1.8.5']
+    }
+
+    @Override
+    Class<? extends XnatInterface> getInterfaceClass() {
+        XnatInterface_1_8_0
+    }
+
+}
