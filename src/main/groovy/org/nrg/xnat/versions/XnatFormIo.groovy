f8169878b231b1ecb69c4663eb68096caaa88e94
Charlie Moore <charlesmoore@flywheel.io>
2022-08-22 10:26:19 -0500

    Adding temporary form io class

---
+++ src/main/groovy/org/nrg/xnat/versions/XnatFormIo.groovy
@@ -0,0 +1,19 @@
+package org.nrg.xnat.versions
+
+import org.nrg.xnat.interfaces.XnatInterface
+import org.nrg.xnat.interfaces.XnatInterface_1_8_0
+
+@Follows(Xnat_1_8_6)
+class XnatFormIo extends XnatVersion {
+
+    @Override
+    List<String> getVersionKeys() {
+        ['formio']
+    }
+
+    @Override
+    Class<? extends XnatInterface> getInterfaceClass() {
+        XnatInterface_1_8_0
+    }
+
+}
