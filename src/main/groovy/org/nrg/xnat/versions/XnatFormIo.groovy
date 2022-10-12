3764d8ab175891d542d8d7b1450c4496ed937001
Charlie Moore <charlesmoore@flywheel.io>
2022-10-12 09:51:28 -0500

    Workaround to exclude form.io tests for now

--- src/main/groovy/org/nrg/xnat/versions/XnatFormIo.groovy
+++ src/main/groovy/org/nrg/xnat/versions/XnatFormIo.groovy
@@ -3,7 +3,7 @@ package org.nrg.xnat.versions
 import org.nrg.xnat.interfaces.XnatInterface
 import org.nrg.xnat.interfaces.XnatInterface_1_8_0
 
-@Follows(Xnat_1_8_6)
+@Follows(Xnat_1_8_7)
 class XnatFormIo extends XnatVersion {
 
     @Override
