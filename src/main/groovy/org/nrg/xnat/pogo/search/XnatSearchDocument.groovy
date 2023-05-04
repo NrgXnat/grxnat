6781535e49b5b89b11e11d49895659db3f4e0811
Charlie Moore <charlesmoore@flywheel.io>
2023-05-04 21:46:45 +0000

    Added some handling in XnatSearchDocument to return a generic version of the XML

--- src/main/groovy/org/nrg/xnat/pogo/search/XnatSearchDocument.groovy
+++ src/main/groovy/org/nrg/xnat/pogo/search/XnatSearchDocument.groovy
@@ -8,6 +8,18 @@ import groovy.transform.builder.SimpleStrategy
 @Builder(builderStrategy = SimpleStrategy, prefix = '')
 class XnatSearchDocument {
 
+    public static final String XML_NAMESPACES_TO_RETAIN = ['arc', 'val', 'pipe', 'wrk', 'scr', 'xdat', 'cat', 'prov', 'xnat', 'xnat_a', 'xsi'].join('|')
+    public static final String NAMESPACE_PATTERN = " (xmlns:(?!${XML_NAMESPACES_TO_RETAIN})\\S+?=\"\\S+?\")|(xsi:schemaLocation=\".*?\") ?"
     String searchXml
+
+    XnatSearchDocument genericize() {
+        searchXml = genericizedXml()
+        this
+    }
+
+    // to strip out things added by plugins
+    String genericizedXml() {
+        searchXml.replaceAll(NAMESPACE_PATTERN, '')
+    }
 
 }
