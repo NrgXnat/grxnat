c451b490485966448f0a9fb9356354ec01d0d5d4
Charlie Moore <charlesmoore@flywheel.io>
2023-08-15 08:47:00 -0500

    Add class for 1.8.10

---
+++ src/main/groovy/org/nrg/xnat/versions/Xnat_1_8_10.groovy
@@ -0,0 +1,19 @@
+package org.nrg.xnat.versions
+
+import org.nrg.xnat.interfaces.XnatInterface
+import org.nrg.xnat.interfaces.XnatInterface_1_8_0
+
+@Follows(Xnat_1_8_9)
+class Xnat_1_8_10 extends XnatVersion {
+
+    @Override
+    List<String> getVersionKeys() {
+        ['1.8.10']
+    }
+
+    @Override
+    Class<? extends XnatInterface> getInterfaceClass() {
+        XnatInterface_1_8_0
+    }
+
+}
