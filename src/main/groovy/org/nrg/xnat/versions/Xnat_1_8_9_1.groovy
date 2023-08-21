84567384c39e84c83f616134cd8b5864529fa310
Charlie Moore <charlesmoore@flywheel.io>
2023-08-21 11:09:40 -0500

    Adding class for 1.8.9.1

--- src/main/groovy/org/nrg/xnat/versions/Xnat_1_8_9_1.groovy
+++ src/main/groovy/org/nrg/xnat/versions/Xnat_1_8_9_1.groovy
@@ -3,12 +3,12 @@ package org.nrg.xnat.versions
 import org.nrg.xnat.interfaces.XnatInterface
 import org.nrg.xnat.interfaces.XnatInterface_1_8_0
 
-@Follows(Xnat_1_8_9_1)
-class Xnat_1_8_10 extends XnatVersion {
+@Follows(Xnat_1_8_9)
+class Xnat_1_8_9_1 extends XnatVersion {
 
     @Override
     List<String> getVersionKeys() {
-        ['1.8.10']
+        ['1.8.9.1']
     }
 
     @Override
