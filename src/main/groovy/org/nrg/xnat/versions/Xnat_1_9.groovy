e2ccbd2975dc71e03b0012822dd27295a905dbca
Rick Herrick <rickherrick@flywheel.io>
2024-09-10 11:03:54 -0500

    Modified Xnat_1_9 to follow Xnat_1_8_11

--- src/main/groovy/org/nrg/xnat/versions/Xnat_1_9.groovy
+++ src/main/groovy/org/nrg/xnat/versions/Xnat_1_9.groovy
@@ -3,7 +3,7 @@ package org.nrg.xnat.versions
 import org.nrg.xnat.interfaces.XnatInterface
 import org.nrg.xnat.interfaces.XnatInterface_1_8_0
 
-@Follows(Xnat_1_8_10)
+@Follows(Xnat_1_8_11)
 class Xnat_1_9 extends XnatVersion {
 
     @Override
