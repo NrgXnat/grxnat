7e884692636e2bd2b94bc81ad7bbe03b54957b6d
Charlie Moore <moore.c@wustl.edu>
2025-03-31 11:50:27 -0500

    Tweaked JSON

--- src/main/groovy/org/nrg/xnat/pogo/plugins/PluginVersionMapping.groovy
+++ src/main/groovy/org/nrg/xnat/pogo/plugins/PluginVersionMapping.groovy
@@ -1,12 +1,8 @@
 package org.nrg.xnat.pogo.plugins
 
-import com.fasterxml.jackson.annotation.JsonValue
-
 class PluginVersionMapping {
     
     String pluginId
-    
-    @JsonValue
     Map<String, List<String>> pluginToXnatVersions
     
 }
