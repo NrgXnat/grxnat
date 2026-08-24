38e618e1dae2add24d96b8c80f62ca2eaa30aa4a
Kate Alpert <alpertk@wustl.edu>
2026-08-24 15:59:40 -0600

    feat: add the 1.10.2 XNAT version (#1)
    Tests gated with @AddedIn(Xnat_1_10_2) need this class to exist. XnatVersionList
    discovers subtypes of XnatVersion by reflection over the package, so following
    Xnat_1_10_1 is all the registration required.

---
+++ src/main/groovy/org/nrg/xnat/versions/Xnat_1_10_2.groovy
@@ -0,0 +1,17 @@
+package org.nrg.xnat.versions
+
+import org.nrg.xnat.interfaces.XnatInterface
+import org.nrg.xnat.interfaces.XnatInterface_1_8_0
+
+@Follows(Xnat_1_10_1)
+class Xnat_1_10_2 extends XnatVersion {
+    @Override
+    List<String> getVersionKeys() {
+        ['1.10.2']
+    }
+
+    @Override
+    Class<? extends XnatInterface> getInterfaceClass() {
+        XnatInterface_1_8_0
+    }
+}
